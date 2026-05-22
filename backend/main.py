6a8e8ee1cfaffd80fd07b8ae441f5a3e38203c83
ANUSHREE-123-S <anushreeshivalinga@gmail.com>
2026-05-22 20:58:16 +0530

    Completed FastAPI Day 3 POST APIs

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,22 @@
+from pydantic import BaseModel
 from fastapi import FastAPI
+app=FastAPI()
+notes=[]
 
-app = FastAPI()
+class Note(BaseModel):
+    title:str
+    content:str
 
-@app.get("/")
-def home():
-    return {"message": "Backend Running"}
-
-@app.get("/hello/{name}")
-def greet(name: str):
-    return {"Message": f"Hello {name}"}
-
-@app.get("/user/{id}")
-def get_user_id(id: int):
-    return {"user_id is": id}
-
-@app.get("/search")
-def search_name(name: str):
-    return {"searched_name": name}
-
-@app.get("/movie")
-def movie_name(name: str):
-    return {"Movie name is": name}
+@app.post("/add-note")
+def add_note(note:Note):
+    notes.append({
+        "title":note.title,
+        "content":note.content
+    })
+    return{
+        "message":"Note added",
+        "notes":notes
+    }
+@app.get("/get-notes")
+def get_notes():
+    return {"notes":notes}
