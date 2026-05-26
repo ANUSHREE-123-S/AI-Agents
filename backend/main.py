e2de03c5627a4cc59a7d62c47abd1b88a8c74d83
ANUSHREE-123-S <anushreeshivalinga@gmail.com>
2026-05-26 18:15:35 +0530

    4 Built CRUD Notes API using FastAPI

--- backend/main.py
+++ backend/main.py
@@ -20,3 +20,23 @@ def add_note(note:Note):
 @app.get("/get-notes")
 def get_notes():
     return {"notes":notes}
+
+@app.put("/update-notes/{index}")
+def update_notes(index:int, note:Note):
+    if index>=len(notes):
+        return{"message":"note not found"}
+    notes[index]= note.dict()
+    return{
+        "message":"notes is updated",
+        "notes":notes
+    }
+
+@app.delete("/delete-notes/{index}")
+def delete_notes(index:int):
+    if index>=len(notes):
+        return {"errorr":"note is not found"}
+    deleted_note=notes.pop(index)
+    return{"message":"note book is deleted",
+           "deleted_note":deleted_note,
+           "notes":notes
+            }
